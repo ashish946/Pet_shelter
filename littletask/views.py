4dc5d3181ce0fd3fc9e3532c4cf072e9180c9f98
Ashish <ashishdll4@gmail.com>
2020-07-30 13:47:33 +0530

    working

--- littletask/views.py
+++ littletask/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from .models import littask
 
 # Create your views here.
+def home(request):
+    t=littask.objects
+    return render(request,'littletask/home.html',{'img':t})
