bf44c4fa332ae545efc59737a20e97d976dc942e
Ashish <ashishdll4@gmail.com>
2020-08-06 00:14:51 +0530

    Slidder added

--- animals/models.py
+++ animals/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.core.validators import MinValueValidator,MaxValueValidator
+import datetime
 
-# Create your models here.
+ANIMAL_CHOICES=(
+    ('Cat','CAT'),
+    ('Dog','DOG'),
+)
+AGE_CHOICES=(
+    ('Young','YOUNG'),
+    ('Old','OLD'),
+)
+
+GENDER_CHOICES=(
+    ('Female','FEMALE'),
+    ('Male','MALE'),
+)
+
+class Animal(models.Model):
+    name=models.CharField(max_length=32)
+    image=models.ImageField(upload_to='media/',null=True)
+    description=models.TextField(max_length=360,null=True)
+    difficulty=models.IntegerField(
+        validators=[MinValueValidator(1),MaxValueValidator(5)],default=0)
+    animal=models.CharField(choices=ANIMAL_CHOICES,max_length=3,null=True)
+    age=models.CharField(choices=AGE_CHOICES,max_length=5,null=True)
+    gender=models.CharField(choices=GENDER_CHOICES,max_length=6,null=True)
+    Sheltered=models.DateTimeField()
+
+
+    def __str__(self):
+        return self.name
--- animals/views.py
+++ animals/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import Animal
 
 def home(request):
-    return render(request,'home.html',{})
+    animals=Animal.objects.all()
+    return render(request,'home.html',{'animals':animals})
 
 def search(request):
     return render(request,'search.html',{})
